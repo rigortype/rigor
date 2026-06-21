2daa9eb9a9c065deda23e3b4eb4692acf96cc587
USAMI Kenta <tadsan@zonu.me>
2026-06-21 18:18:09 +0900

    Add convention spec for method_dispatcher/member_shape_projection
    Oracle gap follow-up: zero direct spec coverage. New spec covers all 7
    public methods (reader_overridden?, instance_index, instance_to_h,
    instance_deconstruct, instance_deconstruct_keys, instance_members,
    instance_with) via an extending test module. 63.4% fused; remaining
    survivors are internal dispatch calls needing RBS-aware stubs.

---
+++ spec/rigor/inference/method_dispatcher/member_shape_projection_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+RSpec.describe Rigor::Inference::MethodDispatcher::MemberShapeProjection do
+  let!(:projector) do
+    mod = described_class
+    Module.new { extend mod }
+  end
+  let(:one) { Rigor::Type::Combinator.constant_of(1) }
+  let(:two) { Rigor::Type::Combinator.constant_of("two") }
+  let(:members) { { x: one, y: two } }
+  let(:instance) do
+    instance_double(Rigor::Type::DataInstance, members: members, class_name: "Point",
+                                               member_names: %i[x y])
+  end
+
+  describe "#reader_overridden?" do
+    it "returns false when class_name is nil" do
+      anon = instance_double(Rigor::Type::DataInstance, class_name: nil)
+      expect(projector.reader_overridden?(anon, :x, nil)).to be(false)
+    end
+
+    it "returns false when scope is nil" do
+      expect(projector.reader_overridden?(instance, :x, nil)).to be(false)
+    end
+  end
+
+  describe "#instance_index" do
+    it "returns the member type for a Symbol key" do
+      arg = Rigor::Type::Combinator.constant_of(:x)
+      expect(projector.instance_index(instance, [arg])).to eq(one)
+    end
+
+    it "returns nil for a non-Constant arg" do
+      arg = Rigor::Type::Combinator.nominal_of("Symbol")
+      expect(projector.instance_index(instance, [arg])).to be_nil
+    end
+
+    it "returns nil for wrong arg count" do
+      expect(projector.instance_index(instance, [])).to be_nil
+    end
+  end
+
+  describe "#instance_to_h" do
+    it "returns a HashShape with the member entries" do
+      result = projector.instance_to_h(instance)
+      expect(result).to be_a(Rigor::Type::HashShape)
+    end
+  end
+
+  describe "#instance_deconstruct" do
+    it "returns a Tuple of member values" do
+      result = projector.instance_deconstruct(instance)
+      expect(result).to be_a(Rigor::Type::Tuple)
+    end
+  end
+
+  describe "#instance_deconstruct_keys" do
+    it "returns nil when args count > 1" do
+      expect(projector.instance_deconstruct_keys(instance, [1, 2])).to be_nil
+    end
+
+    it "returns a HashShape for valid args" do
+      result = projector.instance_deconstruct_keys(instance, [])
+      expect(result).to be_a(Rigor::Type::HashShape)
+    end
+  end
+
+  describe "#instance_members" do
+    it "returns a Tuple of constant member names" do
+      result = projector.instance_members(instance)
+      expect(result).to be_a(Rigor::Type::Tuple)
+    end
+  end
+
+  describe "#instance_with" do
+    it "returns instance when args are empty" do
+      expect(projector.instance_with(instance, [])).to be(instance)
+    end
+
+    it "returns nil for non-HashShape arg" do
+      arg = Rigor::Type::Combinator.nominal_of("String")
+      expect(projector.instance_with(instance, [arg])).to be_nil
+    end
+  end
+end
